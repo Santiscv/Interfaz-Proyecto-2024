84e6d8cbb319cca15d6e83c3a6831acc6cfd9055
Santiago Carranza <carranzasantiago85@gmail.com>
2024-11-20 21:36:39 -0300

    refactorizacion

--- menu.py
+++ menu.py
@@ -1,9 +1,6 @@
 from instruments_details import obtener_lista_guitarras, obtener_detalles_guitarra ,mostrar_guitarras_disponibles
 
 def menu():
-    """
-    Menú principal para interactuar con el usuario.
-    """
     while True:
         try:
             monto_disponible = float(input("Ingrese el monto disponible (0 para salir): "))
